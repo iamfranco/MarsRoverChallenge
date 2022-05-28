48b42a0de73cfa3ed4ee8bdbd729afac8f018b5b
iamfranco <kinfaifrancochan@gmail.com>
2022-05-28 10:48:20 +0100

    add Coordinates class and unit test for Coordinates class

---
+++ MarsRover.Tests/Models/Positions/CoordinatesTests.cs
@@ -0,0 +1,38 @@
+﻿using MarsRover.Models.Positions;
+
+namespace MarsRover.Tests.Models.Positions
+{
+    internal class CoordinatesTests
+    {
+        [Test]
+        public void Constructor_With_Valid_X_Y_Inputs_Should_Succeed()
+        {
+            Coordinates coordinates;
+            Action act;
+
+            act = () => coordinates = new Coordinates(5, 4);
+            act.Should().NotThrow();
+
+            act = () => coordinates = new Coordinates(-1, 3);
+            act.Should().NotThrow();
+
+            act = () => coordinates = new Coordinates(2, -1);
+            act.Should().NotThrow();
+
+            act = () => coordinates = new Coordinates(0, 0);
+            act.Should().NotThrow();
+        }
+
+        [Test]
+        public void Coordinate_Plus_AnotherCoordinate_Should_Return_Coordinate_Of_Vector_Sum()
+        {
+            Coordinates coordinatesA = new(3, 2);
+            Coordinates coordinatesB = new(5, -3);
+
+            Coordinates expectedResult = new(8, -1);
+            Coordinates actualResult = coordinatesA + coordinatesB;
+
+            actualResult.Should().Be(expectedResult);
+        }
+    }
+}
---
+++ MarsRover/Models/Positions/Coordinates.cs
@@ -0,0 +1,18 @@
+﻿namespace MarsRover.Models.Positions
+{
+    public struct Coordinates
+    {
+        private readonly int _x;
+        private readonly int _y;
+
+        public Coordinates(int x, int y)
+        {
+            _x = x;
+            _y = y;
+        }
+
+        public static Coordinates operator +(Coordinates a, Coordinates b) => new(a._x + b._x, a._y + b._y);
+
+        public override string ToString() => $"({_x}, {_y})";
+    }
+}
