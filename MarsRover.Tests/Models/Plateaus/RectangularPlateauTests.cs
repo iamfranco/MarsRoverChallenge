f4322e056741f2dfe9d513a87886041e91ec07ad
iamfranco <kinfaifrancochan@gmail.com>
2022-05-28 16:25:19 +0100

    add RectangularPlateau class and unit test for it

---
+++ MarsRover.Tests/Models/Plateaus/RectangularPlateauTests.cs
@@ -0,0 +1,151 @@
+﻿using MarsRover.Models.Plateaus;
+using MarsRover.Models.Positions;
+
+namespace MarsRover.Tests.Models.Plateaus
+{
+    internal class RectangularPlateauTests
+    {
+        [Test]
+        public void Constructor_Should_Throw_Exception_For_Input_PlateauSize_With_Negative_Components()
+        {
+            RectangularPlateau plateau;
+            Coordinates plateauSize;
+            Action act;
+
+            plateauSize = new(-5, 8);
+            act = () => plateau = new(plateauSize);
+            act.Should().Throw<ArgumentException>();
+
+            plateauSize = new(3, -4);
+            act = () => plateau = new(plateauSize);
+            act.Should().Throw<ArgumentException>();
+        }
+
+        [Test]
+        public void PlateauSize_Should_Return_Constructor_PlateauSize_Input()
+        {
+            RectangularPlateau plateau;
+            Coordinates plateauSize;
+
+            plateauSize = new(5, 5);
+            plateau = new(plateauSize);
+            plateau.PlateauSize.Should().Be(plateauSize);
+
+            plateauSize = new(2, 10);
+            plateau = new(plateauSize);
+            plateau.PlateauSize.Should().Be(plateauSize);
+
+            plateauSize = new(0, 0);
+            plateau = new(plateauSize);
+            plateau.PlateauSize.Should().Be(plateauSize);
+        }
+
+        [Test]
+        public void ObstacleCoordinates_Should_Return_Empty_List_By_Default()
+        {
+            RectangularPlateau plateau = new(new(5, 5));
+            plateau.ObstacleCoordinates.Count.Should().Be(0);
+        }
+
+        [Test]
+        public void AddObstacle_Then_ObstacleCoordinates_Should_Return_AddedObstacleCoordinate()
+        {
+            RectangularPlateau plateau = new(new(5, 5));
+            List<Coordinates> obstacleCoordinates = new()
+            {
+                new(1, 3),
+                new(2, 2),
+                new(3, 4)
+            };
+
+            foreach (Coordinates obstacle in obstacleCoordinates)
+            {
+                plateau.AddObstacle(obstacle);
+            }
+
+            plateau.ObstacleCoordinates.Count.Should().Be(3);
+            for (int i = 0; i < 3; i++)
+            {
+                plateau.ObstacleCoordinates[i].Should().Be(obstacleCoordinates[i]);
+            }
+        }
+
+        [Test]
+        public void AddObstacle_Then_ObstacleCoordinates_Should_Not_Have_Duplicate_Obstacles()
+        {
+            RectangularPlateau plateau = new(new(5, 5));
+
+            plateau.AddObstacle(new(1, 3));
+            plateau.ObstacleCoordinates.Count.Should().Be(1);
+
+            plateau.AddObstacle(new(1, 3));
+            plateau.ObstacleCoordinates.Count.Should().Be(1);
+
+            plateau.AddObstacle(new(2, 2));
+            plateau.ObstacleCoordinates.Count.Should().Be(2);
+
+            plateau.ObstacleCoordinates[0].Should().Be(new Coordinates(1, 3));
+            plateau.ObstacleCoordinates[1].Should().Be(new Coordinates(2, 2));
+        }
+
+        [Test]
+        public void RemoveObstacle_Then_ObstacleCoordinates_Should_Not_Include_Removed_ObstacleCoordinate()
+        {
+            RectangularPlateau plateau = new(new(5, 5));
+            plateau.AddObstacle(new(1, 2));
+            plateau.AddObstacle(new(2, 4));
+
+            plateau.ObstacleCoordinates.Count.Should().Be(2);
+
+            plateau.RemoveObstacle(new(1, 2));
+            plateau.ObstacleCoordinates.Count.Should().Be(1);
+            plateau.ObstacleCoordinates.Contains(new Coordinates(1, 2)).Should().Be(false);
+        }
+
+        [Test]
+        public void RemoveObstacle_With_NonExistent_Obstacle_Should_Not_Change_ObstacleCoordinates()
+        {
+            RectangularPlateau plateau = new(new(5, 5));
+            plateau.AddObstacle(new(1, 2));
+            plateau.AddObstacle(new(2, 4));
+
+            plateau.ObstacleCoordinates.Count.Should().Be(2);
+            plateau.ObstacleCoordinates[0].Should().Be(new Coordinates(1, 2));
+            plateau.ObstacleCoordinates[1].Should().Be(new Coordinates(2, 4));
+
+            plateau.RemoveObstacle(new(2, 2));
+            plateau.ObstacleCoordinates.Count.Should().Be(2);
+            plateau.ObstacleCoordinates[0].Should().Be(new Coordinates(1, 2));
+            plateau.ObstacleCoordinates[1].Should().Be(new Coordinates(2, 4));
+        }
+
+        [Test]
+        public void IsCoordinateValidInPlateau_With_Coordinate_Outside_Of_Plateau_Should_Return_False()
+        {
+            RectangularPlateau plateau = new(new(5, 5));
+            plateau.IsCoordinateValieInPlateau(new(-1, -2)).Should().Be(false);
+            plateau.IsCoordinateValieInPlateau(new(-1, 3)).Should().Be(false);
+            plateau.IsCoordinateValieInPlateau(new(3, 10)).Should().Be(false);
+        }
+
+        [Test]
+        public void IsCoordinateValidInPlateau_With_Coordinate_Inside_Plateau_And_Not_On_Obstacle_Should_Return_True()
+        {
+            RectangularPlateau plateau = new(new(5, 5));
+            plateau.IsCoordinateValieInPlateau(new(4, 3)).Should().Be(true);
+            plateau.IsCoordinateValieInPlateau(new(1, 5)).Should().Be(true);
+            plateau.IsCoordinateValieInPlateau(new(0, 0)).Should().Be(true);
+        }
+
+        [Test]
+        public void IsCoordinateValidInPLateau_With_Coordinate_On_Obstacle_Should_Return_False()
+        {
+            RectangularPlateau plateau = new(new(5, 5));
+            plateau.AddObstacle(new(1, 2));
+            plateau.AddObstacle(new(4, 3));
+
+            plateau.IsCoordinateValieInPlateau(new(1, 2)).Should().Be(false);
+            plateau.IsCoordinateValieInPlateau(new(4, 3)).Should().Be(false);
+        }
+    }
+}
---
+++ MarsRover/Models/Plateaus/RectangularPlateau.cs
@@ -0,0 +1,44 @@
+﻿using MarsRover.Models.Positions;
+using System.Collections.ObjectModel;
+
+namespace MarsRover.Models.Plateaus
+{
+    public class RectangularPlateau
+    {
+        private List<Coordinates> _obstacleCoordinates = new();
+        public Coordinates PlateauSize { get; }
+
+        public ReadOnlyCollection<Coordinates> ObstacleCoordinates => _obstacleCoordinates.AsReadOnly();
+
+        public RectangularPlateau(Coordinates plateauSize)
+        {
+            if (!IsValidPlateauSize(plateauSize))
+                throw new ArgumentException($"{nameof(plateauSize)} {plateauSize} cannot have negative components", nameof(plateauSize));
+
+            PlateauSize = plateauSize;
+        }
+
+        public void AddObstacle(Coordinates obstacle)
+        {
+            _obstacleCoordinates.Add(obstacle);
+            _obstacleCoordinates = _obstacleCoordinates.Distinct().ToList();
+        }
+
+        public void RemoveObstacle(Coordinates obstacle) => _obstacleCoordinates.Remove(obstacle);
+
+        public bool IsCoordinateValieInPlateau(Coordinates coordinates)
+        {
+            if (coordinates.X < 0 || coordinates.X > PlateauSize.X)
+                return false;
+
+            if (coordinates.Y < 0 || coordinates.Y > PlateauSize.Y)
+                return false;
+
+            if (_obstacleCoordinates.Contains(coordinates))
+                return false;
+
+            return true;
+        }
+        private static bool IsValidPlateauSize(Coordinates plateauSize) => plateauSize.X >= 0 && plateauSize.Y >= 0;
+    }
+}
