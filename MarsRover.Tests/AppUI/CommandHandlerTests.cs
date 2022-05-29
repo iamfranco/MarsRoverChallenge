cc449cee7dac532b8cee2400b407e58672e04744
iamfranco <kinfaifrancochan@gmail.com>
2022-05-29 11:21:14 +0100

    add CommandHandler class unimplemented with unit test

---
+++ MarsRover.Tests/AppUI/CommandHandlerTests.cs
@@ -0,0 +1,36 @@
+﻿using MarsRover.AppUI;
+using MarsRover.Models.Instructions;
+using MarsRover.Models.Plateaus;
+using MarsRover.Models.Positions;
+using MarsRover.Models.Vehicles;
+
+namespace MarsRover.Tests.AppUI
+{
+    internal class CommandHandlerTests
+    {
+        CommandHandler commandHandler;
+        [SetUp]
+        public void Setup()
+        {
+            IInstructionReader instructionReader = new StandardInstructionReader();
+            IPositionStringConverter positionStringConverter = new PositionStringConverter();
+            commandHandler = new CommandHandler(instructionReader, positionStringConverter);
+        }
+
+        [Test]
+        public void RequestPosition_Without_ConnectVehicle_Should_Return_No_Vehicle_Connected()
+        {
+            commandHandler.RequestPosition().Should().Be("No Vehicle Connected");
+        }
+
+        [Test]
+        public void RequestPosition_After_ConnectVehicle_Should_Return_Vehicle_Position()
+        {
+            PlateauBase plateau = new RectangularPlateau(new Coordinates(5, 5));
+            VehicleBase vehicle = new Rover("1 2 N", plateau);
+
+            commandHandler.ConnectVehicle(vehicle);
+            commandHandler.RequestPosition().Should().Be("1 2 N");
+        }
+    }
+}
---
+++ MarsRover/AppUI/CommandHandler.cs
@@ -0,0 +1,28 @@
+﻿using MarsRover.Models.Instructions;
+using MarsRover.Models.Positions;
+using MarsRover.Models.Vehicles;
+
+namespace MarsRover.AppUI
+{
+    public class CommandHandler
+    {
+        private IInstructionReader _instructionReader;
+        private IPositionStringConverter _positionStringConverter;
+
+        public CommandHandler(IInstructionReader instructionReader, IPositionStringConverter positionStringConverter)
+        {
+            _instructionReader = instructionReader;
+            _positionStringConverter = positionStringConverter;
+        }
+
+        public string RequestPosition()
+        {
+            return "No Vehicle Connected";
+        }
+
+        public void ConnectVehicle(VehicleBase vehicle)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
