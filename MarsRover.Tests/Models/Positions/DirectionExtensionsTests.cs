ab67833d54cef8dc9a757162a2d53b030761d44d
iamfranco <kinfaifrancochan@gmail.com>
2022-05-30 15:57:10 +0100

    add DirectionExtensions class for DirectionEnum, with unit test

---
+++ MarsRover.Tests/Models/Positions/DirectionExtensionsTests.cs
@@ -0,0 +1,59 @@
+﻿using MarsRover.Models.Positions;
+
+namespace MarsRover.Tests.Models.Positions
+{
+    internal class DirectionExtensionsTests
+    {
+        [Test]
+        public void GetMovementVector_Should_Return_Correct_Movement_Vector()
+        {
+            DirectionEnum north = DirectionEnum.North;
+            north.GetMovementVector().Should().Be(new Coordinates(0, 1));
+
+            DirectionEnum south = DirectionEnum.South;
+            south.GetMovementVector().Should().Be(new Coordinates(0, -1));
+
+            DirectionEnum east = DirectionEnum.East;
+            east.GetMovementVector().Should().Be(new Coordinates(1, 0));
+
+            DirectionEnum west = DirectionEnum.West;
+            west.GetMovementVector().Should().Be(new Coordinates(-1, 0));
+        }
+
+        [Test]
+        public void GetLeftTurn_Should_Modify_Direction_To_CounterClockwise_Direction()
+        {
+            DirectionEnum direction = DirectionEnum.North;
+
+            direction = direction.GetLeftTurn();
+            direction.Should().Be(DirectionEnum.West);
+
+            direction = direction.GetLeftTurn();
+            direction.Should().Be(DirectionEnum.South);
+
+            direction = direction.GetLeftTurn();
+            direction.Should().Be(DirectionEnum.East);
+
+            direction = direction.GetLeftTurn();
+            direction.Should().Be(DirectionEnum.North);
+        }
+
+        [Test]
+        public void GetRightTurn_Should_Modify_Direction_To_CounterClockwise_Direction()
+        {
+            DirectionEnum direction = DirectionEnum.North;
+
+            direction = direction.GetRightTurn();
+            direction.Should().Be(DirectionEnum.East);
+
+            direction = direction.GetRightTurn();
+            direction.Should().Be(DirectionEnum.South);
+
+            direction = direction.GetRightTurn();
+            direction.Should().Be(DirectionEnum.West);
+
+            direction = direction.GetRightTurn();
+            direction.Should().Be(DirectionEnum.North);
+        }
+    }
+}
---
+++ MarsRover/Models/Positions/DirectionExtensions.cs
@@ -0,0 +1,28 @@
+﻿namespace MarsRover.Models.Positions
+{
+    public enum DirectionEnum
+    {
+        North,
+        East,
+        South,
+        West
+    }
+
+    public static class DirectionExtensions
+    {
+        public static Coordinates GetMovementVector(this DirectionEnum direction)
+        {
+            return direction switch
+            {
+                DirectionEnum.North => new(0, 1),
+                DirectionEnum.South => new(0, -1),
+                DirectionEnum.East => new(1, 0),
+                DirectionEnum.West => new(-1, 0),
+                _ => new(0, 0)
+            };
+        }
+
+        public static DirectionEnum GetLeftTurn(this DirectionEnum direction) => (DirectionEnum)(((int)direction + 3) % 4);
+        public static DirectionEnum GetRightTurn(this DirectionEnum direction) => (DirectionEnum)(((int)direction + 1) % 4);
+    }
+}
