c2b51c623a40bbc89976fe7def525b34a92b80ec
iamfranco <kinfaifrancochan@gmail.com>
2022-06-01 12:44:46 +0100

    move unit test for Obstacles into ObstaclesContainerTests, add class ObstaclesContainer and implement methods to make test pass

---
+++ MarsRover/Models/Plateaus/ObstaclesContainer.cs
@@ -0,0 +1,38 @@
+﻿using MarsRover.Models.Positions;
+using System.Collections.ObjectModel;
+
+namespace MarsRover.Models.Plateaus
+{
+    public class ObstaclesContainer
+    {
+        private readonly Func<Coordinates, bool> _coordinateValidateFunc;
+        private readonly List<Coordinates> _obstacleCoordinates;
+
+        public ObstaclesContainer(Func<Coordinates, bool> coordinateValidateFunc)
+        {
+            if (coordinateValidateFunc is null)
+                throw new ArgumentNullException(nameof(coordinateValidateFunc));
+
+            _coordinateValidateFunc = coordinateValidateFunc;
+            _obstacleCoordinates = new();
+        }
+
+        public ReadOnlyCollection<Coordinates> ObstacleCoordinates => _obstacleCoordinates.AsReadOnly();
+
+        public void AddObstacle(Coordinates obstacle)
+        {
+            if (!_coordinateValidateFunc(obstacle))
+                return;
+
+            if (ObstacleCoordinates.Contains(obstacle))
+                return;
+
+            _obstacleCoordinates.Add(obstacle);
+        }
+
+        public void RemoveObstacle(Coordinates obstacle)
+        {
+            _obstacleCoordinates.Remove(obstacle);
+        }
+    }
+}
