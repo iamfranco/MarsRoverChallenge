a39080db0400b5e2641d583d97cb57f41162bce2
iamfranco <kinfaifrancochan@gmail.com>
2022-06-03 19:44:05 +0100

    add unit test for InputReaderContainer, and modify InputReaderContainer to pass test

---
+++ MarsRover.Tests/AppUI/Helpers/InputReaderContainerTests.cs
@@ -0,0 +1,46 @@
+﻿using MarsRover.AppUI.Helpers;
+
+namespace MarsRover.Tests.AppUI.Helpers;
+internal class InputReaderContainerTests
+{
+    [Test]
+    public void InputReaderForTest_Construct_With_Null_Input_Should_Throw_Exception()
+    {
+        InputReaderForTest inputReader;
+        Action act = () => inputReader = new InputReaderForTest(null);
+
+        act.Should().Throw<ArgumentNullException>();
+    }
+
+    [Test]
+    public void InputReaderForTest_Construct_With_Empty_List_Input_Should_Throw_Exception()
+    {
+        List<string> inputs = new();
+        InputReaderForTest inputReader;
+
+        Action act = () => inputReader = new InputReaderForTest(inputs);
+
+        act.Should().Throw<ArgumentException>();
+    }
+
+    [Test]
+    public void SetInputReader_With_Null_Input_Should_Throw_Exception()
+    {
+        Action act = () => InputReaderContainer.SetInputReader(null);
+
+        act.Should().Throw<ArgumentNullException>();
+    }
+
+    [Test]
+    public void GetUserInput_Should_Return_Input_In_Correct_Order()
+    {
+        List<string> inputs = new() { "input 1", "2", "three", "IV" };
+        InputReaderForTest inputReader = new InputReaderForTest(inputs);
+        InputReaderContainer.SetInputReader(inputReader);
+
+        foreach (string input in inputs)
+        {
+            InputReaderContainer.GetUserInput("Enter something: ").Should().Be(input);
+        }
+    }
+}
--- MarsRover/AppUI/Helpers/InputReaderContainer.cs
+++ MarsRover/AppUI/Helpers/InputReaderContainer.cs
@@ -3,7 +3,13 @@ public static class InputReaderContainer
 {
     private static InputReader _inputReader = new();
 
-    public static void SetInputReader(InputReader inputReader) => _inputReader = inputReader;
+    public static void SetInputReader(InputReader inputReader)
+    {
+        if (inputReader is null)
+            throw new ArgumentNullException(nameof(inputReader));
+
+        _inputReader = inputReader;
+    }
 
     public static string GetUserInput(string prompt) => _inputReader.GetUserInput(prompt);
 }
