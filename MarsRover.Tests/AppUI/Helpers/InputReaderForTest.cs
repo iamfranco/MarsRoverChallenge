92f7f6775aa2b465d98db31dcbecb93e3bce3464
iamfranco <kinfaifrancochan@gmail.com>
2022-06-03 19:43:34 +0100

    unit test rename UserInputReaderForTest class to InputReaderForTest

---
+++ MarsRover.Tests/AppUI/Helpers/InputReaderForTest.cs
@@ -0,0 +1,21 @@
+﻿using MarsRover.AppUI.Helpers;
+
+namespace MarsRover.Tests.AppUI.Helpers;
+internal class InputReaderForTest : InputReader
+{
+    private readonly List<string> _inputs = new();
+    private int index = 0;
+
+    public InputReaderForTest(List<string> inputs)
+    {
+        if (inputs is null)
+            throw new ArgumentNullException(nameof(inputs));
+
+        if (inputs.Count == 0)
+            throw new ArgumentException($"{nameof(inputs)} cannot be empty");
+
+        _inputs = inputs;
+    }
+
+    public override string GetUserInput(string prompt) => _inputs[index++];
+}
