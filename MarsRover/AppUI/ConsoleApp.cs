66ef4df3a478bca0384c49c3065a5be94daf1e76
iamfranco <kinfaifrancochan@gmail.com>
2022-06-04 15:32:19 +0100

    move Program.cs app UI structure code into ConsoleApp.cs

---
+++ MarsRover/AppUI/ConsoleApp.cs
@@ -0,0 +1,33 @@
+﻿using MarsRover.Models.Elementals;
+using MarsRover.Models.Plateaus;
+using MarsRover.Models.Vehicles;
+
+namespace MarsRover.AppUI;
+public static class ConsoleApp
+{
+    public static void Run(AppUIHandler appUIHandler,
+        Dictionary<string, Func<PlateauBase>> plateauMakers,
+        Dictionary<string, Func<Position, VehicleBase>> vehicleMakers)
+    {
+        try
+        {
+            appUIHandler.AskUserToMakePlateau(plateauMakers);
+            appUIHandler.AskUserToMakeObstacles();
+
+            while (true)
+            {
+                appUIHandler.AskUserToCreateNewVehicleOrConnectToExistingVehicle(vehicleMakers);
+                appUIHandler.AskUserForMovementInstructionAndSendToVehicle();
+
+                Console.WriteLine();
+                Console.Write("Press any key to continue.. ");
+                Console.ReadKey();
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+            Console.ReadLine();
+        }
+    }
+}
